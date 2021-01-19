90ff5ca4d2611166d7b0132305a885d8c96d707a
ruffianjiang <jiangyj0516@outlook.com>
2021-01-19 18:14:10 +0800

    init

---
+++ jvmloader/src/main/java/com/dawn/jvmstart/jvmloader/JvmloaderApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.jvmstart.jvmloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmloaderApplication.class, args);
+    }
+
+}
---
+++ jvmloader/src/test/java/com/dawn/jvmstart/jvmloader/JvmloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.jvmstart.jvmloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
